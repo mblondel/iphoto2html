001fc531f8e2ef5e03a1506e0c1d053ecc8d0635
Mathieu Blondel <mathieu@mblondel.org>
2014-05-11 13:49:29 +0900

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+# Please run
+# python setup.py install
+
+setup(
+    name = 'iphoto2html',
+    description = 'Convert your iPhoto library to a static web gallery',
+    author = 'Mathieu Blondel',
+    author_email = 'mathieu ÂT mblondel DÔT org',
+    url = 'https://github.com/mblondel/iphoto2html',
+    version = '0.1',
+    license='Apache 2.0',
+    packages = ['iphoto2html'],
+    package_dir = {'iphoto2html': 'iphoto2html'},
+    package_data = {'iphoto2html': ['data/*.*']},
+    scripts = ['bin/iphoto2html'],
+
+)
